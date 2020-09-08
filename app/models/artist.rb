f1143de84a35166d896561a1de1e8e0705571f91
brynknowles <bryn.knowles@gmail.com>
2020-09-08 10:31:40 -0400

    completed lab

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -4,13 +4,16 @@ class Artist < ActiveRecord::Base
 
   def get_genre_of_first_song
     #return the genre of the artist's first saved song
+    self.songs.map {|song| song.genre}.first
   end
 
   def song_count
     #return the number of songs associated with the artist
+    self.songs.select {|song| song.artist == self}.length
   end
 
   def genre_count
     #return the number of genres associated with the artist
+    self.songs.select {|song| song.genre}.length
   end
 end
